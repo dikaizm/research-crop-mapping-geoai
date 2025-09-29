3c0d6fa219a81da5e338987c931dcb5470033251
dikaizm <dikamaah@gmail.com>
2025-09-30 02:17:17 +0700

    feat: new model experiments

---
+++ utils/constants.py
@@ -0,0 +1,136 @@
+USDA_CDL_COLORS = {
+    0:  "#000000",  # Background
+    1:  "#ffd400",  # Corn
+    2:  "#ff2626",  # Cotton
+    3:  "#00a9e6",  # Rice
+    4:  "#ff9e0f",  # Sorghum
+    5:  "#267300",  # Soybeans
+    6:  "#ffff00",  # Sunflower
+    10: "#70a800",  # Peanuts
+    11: "#00af4d",  # Tobacco
+    12: "#e0a60f",  # Sweet Corn
+    13: "#e0a60f",  # Pop or Orn Corn
+    14: "#80d4ff",  # Mint
+    21: "#e2007f",  # Barley
+    22: "#8a6453",  # Durum Wheat
+    23: "#d9b56c",  # Spring Wheat
+    24: "#a87000",  # Winter Wheat
+    25: "#d69dbc",  # Other Small Grains
+    26: "#737300",  # Dbl Crop WinWht/Soybeans
+    27: "#ae017e",  # Rye
+    28: "#a15889",  # Oats
+    29: "#73004c",  # Millet
+    30: "#d69dbc",  # Speltz
+    31: "#d1ff00",  # Canola
+    32: "#8099ff",  # Flaxseed
+    33: "#d6d600",  # Safflower
+    34: "#d1ff00",  # Rape Seed
+    35: "#00af4d",  # Mustard
+    36: "#ffa8e3",  # Alfalfa
+    37: "#a5f58d",  # Other Hay/Non Alfalfa
+    38: "#00af4d",  # Camelina
+    39: "#d69dbc",  # Buckwheat
+    41: "#a900e6",  # Sugarbeets
+    42: "#a80000",  # Dry Beans
+    43: "#732600",  # Potatoes
+    44: "#00af4d",  # Other Crops
+    45: "#b380ff",  # Sugarcane
+    46: "#732600",  # Sweet Potatoes
+    47: "#ff6666",  # Misc Vegs & Fruits
+    48: "#ff6666",  # Watermelons
+    49: "#ffcc66",  # Onions
+    50: "#ff6666",  # Cucumbers
+    51: "#00af4d",  # Chick Peas
+    52: "#00deb0",  # Lentils
+    53: "#55ff00",  # Peas
+    54: "#f5a27a",  # Tomatoes
+    55: "#ff6666",  # Caneberries
+    56: "#00af4d",  # Hops
+    57: "#80d4ff",  # Herbs
+    58: "#e8beff",  # Clover/Wildflowers
+    59: "#b2ffde",  # Sod/Grass Seed
+    60: "#00af4d",  # Switchgrass
+    61: "#bfbf7a",  # Fallow/Idle Cropland
+    63: "#95ce93",  # Forest
+    64: "#c7d79e",  # Shrubland
+    65: "#ccbfa3",  # Barren
+    66: "#ff00ff",  # Cherries
+    67: "#ff91ab",  # Peaches
+    68: "#b90050",  # Apples
+    69: "#704489",  # Grapes
+    70: "#007878",  # Christmas Trees
+    71: "#b39c70",  # Other Tree Crops
+    72: "#ffff80",  # Citrus
+    74: "#b6705c",  # Pecans
+    75: "#00a884",  # Almonds
+    76: "#ebd6b0",  # Walnuts
+    77: "#b39c70",  # Pears
+    81: "#f7f7f7",  # Clouds/No Data
+    82: "#9c9c9c",  # Developed
+    83: "#4d70a3",  # Water
+    87: "#80b3b3",  # Wetlands
+    88: "#e9ffbe",  # Nonag/Undefined
+    92: "#00ffff",  # Aquaculture
+    111: "#4d70a3", # Open Water
+    112: "#d4e3fc", # Perennial Ice/Snow
+    121: "#9c9c9c", # Developed/Open Space
+    122: "#9c9c9c", # Developed/Low Intensity
+    123: "#9c9c9c", # Developed/Med Intensity
+    124: "#9c9c9c", # Developed/High Intensity
+    131: "#ccbfa3", # Barren
+    141: "#95ce93", # Deciduous Forest
+    142: "#95ce93", # Evergreen Forest
+    143: "#95ce93", # Mixed Forest
+    152: "#c7d79e", # Shrubland
+    176: "#e9ffbe", # Grass/Pasture
+    190: "#80b3b3", # Woody Wetlands
+    195: "#80b3b3", # Herbaceous Wetlands
+    204: "#00ff8c", # Pistachios
+    205: "#d69dbc", # Triticale
+    206: "#ff6666", # Carrots
+    207: "#ff6666", # Asparagus
+    208: "#ff6666", # Garlic
+    209: "#ff6666", # Cantaloupes
+    210: "#ff91ab", # Prunes
+    211: "#344a34", # Olives
+    212: "#e67525", # Oranges
+    213: "#ff6666", # Honeydew Melons
+    214: "#ff6666", # Broccoli
+    215: "#66994d", # Avocados
+    216: "#ff6666", # Peppers
+    217: "#b39c70", # Pomegranates
+    218: "#ff91ab", # Nectarines
+    219: "#ff6666", # Greens
+    220: "#ff91ab", # Plums
+    221: "#ff6666", # Strawberries
+    222: "#ff6666", # Squash
+    223: "#ff91ab", # Apricots
+    224: "#00af4d", # Vetch
+    225: "#ffd400", # Dbl Crop WinWht/Corn
+    226: "#ffd400", # Dbl Crop Oats/Corn
+    227: "#ff6666", # Lettuce
+    228: "#ffd400", # Dbl Crop Triticale/Corn
+    229: "#ff6666", # Pumpkins
+    230: "#8a6453", # Dbl Crop Lettuce/Durum Wht
+    231: "#ff6666", # Dbl Crop Lettuce/Cantaloupe
+    232: "#ff2626", # Dbl Crop Lettuce/Cotton
+    233: "#e2007f", # Dbl Crop Lettuce/Barley
+    234: "#ff9e0f", # Dbl Crop Durum Wht/Sorghum
+    235: "#ff9e0f", # Dbl Crop Barley/Sorghum
+    236: "#a87000", # Dbl Crop WinWht/Sorghum
+    237: "#ffd400", # Dbl Crop Barley/Corn
+    238: "#a87000", # Dbl Crop WinWht/Cotton
+    239: "#267300", # Dbl Crop Soybeans/Cotton
+    240: "#267300", # Dbl Crop Soybeans/Oats
+    241: "#ffd400", # Dbl Crop Corn/Soybeans
+    242: "#000099", # Blueberries
+    243: "#ff6666", # Cabbage
+    244: "#ff6666", # Cauliflower
+    245: "#ff6666", # Celery
+    246: "#ff6666", # Radishes
+    247: "#ff6666", # Turnips
+    248: "#ff6666", # Eggplants
+    249: "#ff6666", # Gourds
+    250: "#ff6666", # Cranberries
+    254: "#267300", # Dbl Crop Barley/Soybeans
+}
